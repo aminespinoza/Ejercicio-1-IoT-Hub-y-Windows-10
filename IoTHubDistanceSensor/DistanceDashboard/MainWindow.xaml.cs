542813e47e194f7c162f6de7b2a9b4bfbd685610
Amin Espinoza <doomsday5@hotmail.com>
2017-05-01 15:09:34 -0500

    Commit 2
    Aplicación renovada, con monitor de actividad desde una aplicación WPF.

---
+++ IoTHubDistanceSensor/DistanceDashboard/MainWindow.xaml.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace DistanceDashboard
+{
+    public partial class MainWindow : Window
+    {
+        static ServiceClient serviceClient;
+        static string connectionString = "HostName=demoHubAmin.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=rLxml4G8W5Il00YfJdAESySJ6ioB7yxLfLjM8vncmDc=";
+        static string iotHubD2cEndpoint = "messages/events";
+        static EventHubClient eventHubClient;
+        EventHubReceiver eventHubReceiver;
+        DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+            eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver("1", DateTime.UtcNow);
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private async void Timer_Tick(object sender, EventArgs e)
+        {
+            EventData eventData = await eventHubReceiver.ReceiveAsync();
+            string incomingData = Encoding.UTF8.GetString(eventData.GetBytes());
+            dynamic finalData = JObject.Parse(incomingData);
+
+            int distance = Convert.ToInt16(finalData.distance);
+            HandleDistance(distance);
+        }
+
+        private void HandleDistance(int distance)
+        {
+            if (distance > 190)
+            {
+                rect1.Visibility = Visibility.Visible;
+                rect2.Visibility = Visibility.Collapsed;
+                rect3.Visibility = Visibility.Collapsed;
+                rect4.Visibility = Visibility.Collapsed;
+                rect5.Visibility = Visibility.Collapsed;
+                rect6.Visibility = Visibility.Collapsed;
+                rect7.Visibility = Visibility.Collapsed;
+                rect8.Visibility = Visibility.Collapsed;
+                rect9.Visibility = Visibility.Collapsed;
+                rect10.Visibility = Visibility.Collapsed;
+            }
+            else if (distance < 189 && distance > 170)
+            {
+                rect1.Visibility = Visibility.Visible;
+                rect2.Visibility = Visibility.Visible;
+                rect3.Visibility = Visibility.Collapsed;
+                rect4.Visibility = Visibility.Collapsed;
+                rect5.Visibility = Visibility.Collapsed;
+                rect6.Visibility = Visibility.Collapsed;
+                rect7.Visibility = Visibility.Collapsed;
+                rect8.Visibility = Visibility.Collapsed;
+                rect9.Visibility = Visibility.Collapsed;
+                rect10.Visibility = Visibility.Collapsed;
+            }
+            else if (distance < 169 && distance > 150)
+            {
+                rect1.Visibility = Visibility.Visible;
+                rect2.Visibility = Visibility.Visible;
+                rect3.Visibility = Visibility.Visible;
+                rect4.Visibility = Visibility.Collapsed;
+                rect5.Visibility = Visibility.Collapsed;
+                rect6.Visibility = Visibility.Collapsed;
+                rect7.Visibility = Visibility.Collapsed;
+                rect8.Visibility = Visibility.Collapsed;
+                rect9.Visibility = Visibility.Collapsed;
+                rect10.Visibility = Visibility.Collapsed;
+            }
+            else if (distance < 149 && distance > 130)
+            {
+                rect1.Visibility = Visibility.Visible;
+                rect2.Visibility = Visibility.Visible;
+                rect3.Visibility = Visibility.Visible;
+                rect4.Visibility = Visibility.Visible;
+                rect5.Visibility = Visibility.Collapsed;
+                rect6.Visibility = Visibility.Collapsed;
+                rect7.Visibility = Visibility.Collapsed;
+                rect8.Visibility = Visibility.Collapsed;
+                rect9.Visibility = Visibility.Collapsed;
+                rect10.Visibility = Visibility.Collapsed;
+            }
+            else if (distance < 129 && distance > 110)
+            {
+                rect1.Visibility = Visibility.Visible;
+                rect2.Visibility = Visibility.Visible;
+                rect3.Visibility = Visibility.Visible;
+                rect4.Visibility = Visibility.Visible;
+                rect5.Visibility = Visibility.Visible;
+                rect6.Visibility = Visibility.Collapsed;
+                rect7.Visibility = Visibility.Collapsed;
+                rect8.Visibility = Visibility.Collapsed;
+                rect9.Visibility = Visibility.Collapsed;
+                rect10.Visibility = Visibility.Collapsed;
+            }
+            else if (distance < 109 && distance > 90)
+            {
+                rect1.Visibility = Visibility.Visible;
+                rect2.Visibility = Visibility.Visible;
+                rect3.Visibility = Visibility.Visible;
+                rect4.Visibility = Visibility.Visible;
+                rect5.Visibility = Visibility.Visible;
+                rect6.Visibility = Visibility.Visible;
+                rect7.Visibility = Visibility.Collapsed;
+                rect8.Visibility = Visibility.Collapsed;
+                rect9.Visibility = Visibility.Collapsed;
+                rect10.Visibility = Visibility.Collapsed;
+            }
+            else if (distance < 89 && distance > 70)
+            {
+                rect1.Visibility = Visibility.Visible;
+                rect2.Visibility = Visibility.Visible;
+                rect3.Visibility = Visibility.Visible;
+                rect4.Visibility = Visibility.Visible;
+                rect5.Visibility = Visibility.Visible;
+                rect6.Visibility = Visibility.Visible;
+                rect7.Visibility = Visibility.Visible;
+                rect8.Visibility = Visibility.Collapsed;
+                rect9.Visibility = Visibility.Collapsed;
+                rect10.Visibility = Visibility.Collapsed;
+            }
+            else if (distance < 69 && distance > 50)
+            {
+                rect1.Visibility = Visibility.Visible;
+                rect2.Visibility = Visibility.Visible;
+                rect3.Visibility = Visibility.Visible;
+                rect4.Visibility = Visibility.Visible;
+                rect5.Visibility = Visibility.Visible;
+                rect6.Visibility = Visibility.Visible;
+                rect7.Visibility = Visibility.Visible;
+                rect8.Visibility = Visibility.Visible;
+                rect9.Visibility = Visibility.Collapsed;
+                rect10.Visibility = Visibility.Collapsed;
+            }
+            else if (distance < 49 && distance > 30)
+            {
+                rect1.Visibility = Visibility.Visible;
+                rect2.Visibility = Visibility.Visible;
+                rect3.Visibility = Visibility.Visible;
+                rect4.Visibility = Visibility.Visible;
+                rect5.Visibility = Visibility.Visible;
+                rect6.Visibility = Visibility.Visible;
+                rect7.Visibility = Visibility.Visible;
+                rect8.Visibility = Visibility.Visible;
+                rect9.Visibility = Visibility.Visible;
+                rect10.Visibility = Visibility.Collapsed;
+            }
+            else if (distance < 29 && distance > 0)
+            {
+                rect1.Visibility = Visibility.Visible;
+                rect2.Visibility = Visibility.Visible;
+                rect3.Visibility = Visibility.Visible;
+                rect4.Visibility = Visibility.Visible;
+                rect5.Visibility = Visibility.Visible;
+                rect6.Visibility = Visibility.Visible;
+                rect7.Visibility = Visibility.Visible;
+                rect8.Visibility = Visibility.Visible;
+                rect9.Visibility = Visibility.Visible;
+                rect10.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ IoTHubDistanceSensor/IoTHubDistanceSensor/MainPage.xaml.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace IoTHubDistanceSensor
+{
+    public sealed partial class MainPage : Page
+    {
+        private DispatcherTimer temporizador = new DispatcherTimer();
+
+        GpioPin echoPin = null; //ECHO
+        GpioPin triggerPin = null; //TRIGGER
+
+        static DeviceClient deviceClient;
+        static string iotHubUri = "demoHubAmin.azure-devices.net";
+        static string deviceKey = "11/uS3+aPjTKc9WdWUJG/yueMEqvbIuN8ckfcB2e+y8=";
+        static string deviceId = "dispositivo001";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            deviceClient = DeviceClient.Create(iotHubUri, AuthenticationMethodFactory.CreateAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey), TransportType.Http1);
+
+            InitGpio();
+
+            temporizador.Interval = TimeSpan.FromSeconds(1);
+            temporizador.Tick += temporizador_Tick;
+            temporizador.Start();
+        }
+
+        private void InitGpio()
+        {
+            var gpioControler = GpioController.GetDefault();
+
+            echoPin = gpioControler.OpenPin(6);
+            triggerPin = gpioControler.OpenPin(13);
+            echoPin.SetDriveMode(GpioPinDriveMode.Input);
+            triggerPin.SetDriveMode(GpioPinDriveMode.Output);
+            triggerPin.Write(GpioPinValue.Low);
+            var value = triggerPin.Read();
+        }
+
+        private void temporizador_Tick(object sender, object e)
+        {
+            var distance = GetDistance();
+            txtDistancia.Text = String.Format("Distancia: {0} cms.", distance);
+
+            EnviarInformaciónAlHub(distance);
+        }
+
+        public int GetDistance()
+        {
+            ManualResetEvent mre = new ManualResetEvent(false);
+            mre.WaitOne(500);
+            Stopwatch pulseLength = new Stopwatch();
+
+            triggerPin.Write(GpioPinValue.High);
+            mre.WaitOne(TimeSpan.FromMilliseconds(0.01));
+            triggerPin.Write(GpioPinValue.Low);
+
+            while (echoPin.Read() == GpioPinValue.Low)
+            {
+            }
+
+            pulseLength.Start();
+
+            while (echoPin.Read() == GpioPinValue.High)
+            {
+            }
+            pulseLength.Stop();
+
+            TimeSpan timeBetween = pulseLength.Elapsed;
+            double distance = timeBetween.TotalSeconds * 17000;
+
+            return Convert.ToInt32(distance);
+        }
+
+        private async void EnviarInformaciónAlHub(int distance)
+        {
+            var telemetryDataPoint = new
+            {
+                deviceId = deviceId,
+                distance = distance,
+                date = DateTime.Now
+            };
+
+            var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+            var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+            Debug.WriteLine(messageString);
+            await deviceClient.SendEventAsync(message);
+            //txtEnvioDatos.Text = "Datos enviados";
+        }
+    }
+}
